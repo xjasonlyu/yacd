e9792978753d6cd79f15fbd0c5f7543a25166011
xjasonlyu <xjasonlyu@gmail.com>
2021-02-12 16:01:24 +0800

    chore: hard-code host address

--- src/store/app.ts
+++ src/store/app.ts
@@ -160,7 +160,7 @@ export function updateCollapsibleIsOpen(
 }
 
 const defaultClashAPIConfig = {
-  baseURL: document.getElementById('app')?.getAttribute('data-base-url') ?? 'http://127.0.0.1:9090',
+  baseURL: document.getElementById('app')?.getAttribute('data-base-url') ?? 'http://10.0.0.7:9090',
   secret: '',
   addedAt: 0,
 };
